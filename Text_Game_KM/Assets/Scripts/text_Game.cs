1a77a143b3ca56b5ecf9007158f43b96294e924e
karminbi3 <karolina1902km@gmail.com>
2023-05-26 19:04:24 +0200

    Update some code

--- Text_Game_KM/Assets/Scripts/text_Game.cs
+++ Text_Game_KM/Assets/Scripts/text_Game.cs
@@ -9,23 +9,43 @@ public class text_Game : MonoBehaviour
     //Gracz odpowiada mniejsza, wieksza lub rowna.
     //ALGORYTM -> wyszukiwanie binarnie ->
 
-  
+
+    int min = 1;
+    int max = 1000;
+    int shoot = 500;
+
     void Start()
     {
-        Debug.Log("Witam w grze - pomyœl o liczbie od 1 do 1000");
+        Debug.Log("Witam w grze - pomyœl o liczbie od "+min+ "do" + max);
+        max = 1001;
         Debug.Log("Czy twoja liczba jest mniejsza/ rowna/ wieksza od 500?");
-        //print string
-        //górna strza³ka by odpowiedziec ¿e wiêksza
-        if (Input.GetKeyDown(KeyCode.UpArrow)) ;
-        {
-            Debug.Log("hje");
-        }
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = shoot;
+            NextShoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = shoot;
+            NextShoot();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Komputer zgadl liczbe");
+        }
     }
+
+    void NextShoot()
+    {
+        shoot = (min + max) / 2;
+        Debug.Log("Czy twoja liczba jest wieksza/rowna/ mniejsza od " + shoot + " ?");
+    }
+
 }
